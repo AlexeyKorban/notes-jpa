b6160510e0137d0165e42aad36ec0eda6be64ffe
AlexeyKorban <ldwx@mail.ru>
2019-02-21 19:10:19 +0500

    Initial commit

---
+++ src/main/java/ru/ldwx/notesjpa/NotesJpaApplication.java
@@ -0,0 +1,13 @@
+package ru.ldwx.notesjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotesJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotesJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/ldwx/notesjpa/NotesJpaApplicationTests.java
@@ -0,0 +1,16 @@
+package ru.ldwx.notesjpa;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NotesJpaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
